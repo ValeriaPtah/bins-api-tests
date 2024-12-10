b661ba1f804a16cac6a591df74d6ce140f9f38cd
Valeria Ptah <avramenko.valerie@gmail.com>
2024-12-11 00:44:10 +0100

    Upgraded Gradle build setup
    Updated Grade version to 8.11.1, added dependencies with latest versions. Some vulnerabilities are present in Moshi library, so it's a temp solution.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,27 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
+//Versions
+val commonsLang3Version = "3.15.0"
+val lombokVersion = "1.18.34"
+val moshiVersion = "1.15.1"
+val restAssuredVersion = "5.5.0"
+val testNgVersion = "7.10.2"
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.3"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testng:testng:$testNgVersion")
+    testImplementation("org.apache.commons:commons-lang3:$commonsLang3Version")
+    testImplementation("org.projectlombok:lombok:$lombokVersion")
+    testImplementation("com.squareup.moshi:moshi:$moshiVersion")
+    testImplementation("com.squareup.moshi:moshi-adapters:$moshiVersion")
+    testImplementation("io.rest-assured:rest-assured:$restAssuredVersion")
+    testImplementation("io.rest-assured:json-schema-validator:$restAssuredVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useTestNG()
 }
